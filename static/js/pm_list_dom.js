1128866813f75046af7edd3c5f47642583c4f8d0
Steve Howell <showell@zulipchat.com>
2020-01-16 17:28:55 +0000

    Make vdom deeper.

--- static/js/pm_list_dom.js
+++ static/js/pm_list_dom.js
@@ -1,21 +1,126 @@
-const render_pm_list_item = require('../templates/pm_list_item.hbs');
+exports.circle_background = (convo) => {
+    if (convo.fraction_present) {
+        return 'background:hsla(106, 74%, 44%, ' + convo.fraction_present + ');';
+    }
+    return 'background:none; border-color:hsl(0, 0%, 50%);';
+};
+
+exports.pm_left_col_span = (convo) => {
+    const classes = [
+        'user_circle',
+        convo.user_circle_class,
+    ];
+    const class_ = classes.join(' ');
+
+    if (convo.is_group) {
+        return vdom.span({
+            attrs: [
+                ['class', class_],
+                ['style', exports.circle_background(convo)],
+            ],
+        });
+    }
+
+    // regular PMs
+    return vdom.span({
+        attrs: [
+            ['class', class_],
+        ],
+    });
+};
+
+exports.pm_left_col = (convo) => {
+    return vdom.div({
+        attrs: [
+            ['class', 'pm_left_col'],
+        ],
+        children: [
+            exports.pm_left_col_span(convo),
+        ],
+    });
+};
+
+exports.partners = (convo) => {
+    return vdom.a({
+        attrs: [
+            ['href', convo.url],
+            ['class', 'conversation-partners'],
+        ],
+        text: convo.recipients,
+    });
+};
+
+exports.count = (convo) => {
+    const classes = ['private_message_count'];
+
+    if (convo.is_zero) {
+        classes.push('zero_count');
+    }
+    const class_ = classes.join(' ');
+
+    return vdom.div({
+        attrs: [
+            ['class', class_],
+        ],
+        children: [
+            vdom.div({
+                attrs: [
+                    ['class', 'value'],
+                ],
+                text: convo.unread,
+            }),
+        ],
+    });
+};
+
+exports.pm_box_attrs = (convo) => {
+    return [
+        ['class', 'pm-box'],
+        ['id', 'pm_user_status'],
+        ['data-user-ids-string', convo.user_ids_string],
+        ['data-is-group', convo.is_group],
+    ];
+};
+
+exports.pm_box = (convo) => {
+    return vdom.span({
+        attrs: exports.pm_box_attrs(convo),
+        children: [
+            exports.pm_left_col(convo),
+            exports.partners(convo),
+            exports.count(convo),
+        ],
+    });
+};
+
+exports.pm_li_class = (convo) => {
+    const classes = [
+        'top_left_row',
+        'expanded_private_messages',
+    ];
+    if (convo.is_zero) {
+        classes.push('zero-pm-unreads');
+    }
+    if (convo.is_active) {
+        classes.push('active-sub-filter');
+    }
+    return classes.join(' ');
+};
 
 exports.keyed_pm_li = (convo) => {
-    const render = () => {
-        return render_pm_list_item(convo);
-    };
-
-    const eq = (other) => {
-        return _.isEqual(convo, other.convo);
-    };
-
     const key = convo.user_ids_string;
+    const attrs = [
+        ['class', exports.pm_li_class(convo)],
+        ['data-user-ids-string', key],
+    ];
+    const ast = vdom.li({
+        attrs: attrs,
+        children: [exports.pm_box(convo)],
+    });
 
     return {
         key: key,
-        render: render,
-        convo: convo,
-        eq: eq,
+        ast: ast,
     };
 };
 
@@ -26,7 +131,7 @@ exports.pm_ul = (convos) => {
     ];
     return vdom.ul({
         attrs: attrs,
-        keyed_nodes: _.map(convos, exports.keyed_pm_li),
+        keyed_children: _.map(convos, exports.keyed_pm_li),
     });
 };
 
--- static/js/vdom.js
+++ static/js/vdom.js
@@ -18,31 +18,94 @@ exports.eq_array = (a, b, eq) => {
     });
 };
 
-exports.ul = (opts) => {
+exports.eq_attr = (a, b) => {
+    return a[0] === b[0] && a[1] === b[1];
+};
+
+exports.eq_keyed_child = (a, b) => {
+    return a.key === b.key &&
+        exports.eq_tag(a.ast, b.ast);
+};
+
+exports.eq_tag = (tag1, tag2) => {
+    if (tag2 === undefined) {
+        return false;
+    }
+
+    if (tag1.tag_name !== tag2.tag_name) {
+        return false;
+    }
+
+    const a = tag1.opts;
+    const b = tag2.opts;
+
+    return a.text === b.text &&
+        exports.eq_array(
+            a.attrs, b.attrs, exports.eq_attr) &&
+        exports.eq_array(
+            a.children, b.children, exports.eq_tag) &&
+        exports.eq_array(
+            a.keyed_children, b.keyed_children, exports.eq_keyed_child);
+};
+
+exports.tag = (tag_name, opts) => {
     return {
-        tag_name: 'ul',
+        tag_name: tag_name,
         opts: opts,
     };
 };
+
+const tags = [
+    'a',
+    'div',
+    'li',
+    'span',
+    'ul',
+];
+
+_.each(tags, (tag_name) => {
+    exports[tag_name] = (opts) => {
+        return exports.tag(tag_name, opts);
+    };
+});
+
+function fix_data(s) {
+    if (typeof s === 'string') {
+        return s;
+    }
+    if (typeof s === 'number') {
+        return s.toString();
+    }
+    return JSON.stringify(s);
+}
 
 exports.render = (tag) => {
     const opts = tag.opts;
     const tag_name = tag.tag_name;
     const attr_str = _.map(opts.attrs, (attr) => {
-        return ' ' + attr[0] + '="' + attr[1] + '"';
+        const val = attr[0].startsWith('data-') ?
+            fix_data(attr[1]) : attr[1];
+        return ' ' + attr[0] + '="' + val + '"';
     }).join('');
 
     const start_tag = '<' + tag_name + attr_str + '>';
     const end_tag = '</' + tag_name + '>';
 
-    if (opts.keyed_nodes === undefined) {
-        blueslip.error("We need keyed_nodes to render innards.");
+    if (opts.keyed_children) {
+        const innards = _.map(opts.keyed_children, (child) => {
+            return exports.render(child.ast);
+        }).join('\n');
+        return start_tag + '\n' + innards + '\n' + end_tag;
+    } else if (opts.children) {
+        const innards = _.map(opts.children, (child) => {
+            return exports.render(child);
+        }).join('\n');
+        return start_tag + '\n' + innards + '\n' + end_tag;
+    } else if (opts.text) {
+        const escaped_text = util.escape_html(opts.text.toString());
+        return start_tag + escaped_text + end_tag;
     }
-
-    const innards = _.map(opts.keyed_nodes, (node) => {
-        return node.render();
-    }).join('\n');
-    return start_tag + '\n' + innards + '\n' + end_tag;
+    return start_tag + end_tag;
 };
 
 exports.update = (container, new_dom, old_dom) => {
@@ -59,17 +122,21 @@ exports.update = (container, new_dom, old_dom) => {
     const new_opts = new_dom.opts;
     const old_opts = old_dom.opts;
 
-    if (new_opts.keyed_nodes === undefined) {
+    if (new_opts.keyed_children === undefined) {
         // We generally want to use vdom on lists, and
         // adding keys for childrens lets us avoid unnecessary
         // redraws (or lets us know we should just rebuild
         // the dom).
-        blueslip.error("We need keyed_nodes for updates.");
+        blueslip.error("We need keyed_children for updates.");
+    }
+
+    if (exports.eq_tag(new_dom, old_dom)) {
+        return;
     }
 
     const same_structure = exports.eq_array(
-        new_opts.keyed_nodes,
-        old_opts.keyed_nodes,
+        new_opts.keyed_children,
+        old_opts.keyed_children,
         (a, b) => a.key === b.key
     );
 
@@ -84,12 +151,12 @@ exports.update = (container, new_dom, old_dom) => {
     const tag_name = new_dom.tag_name;
     const child_elems = container.find(tag_name).children();
 
-    _.each(new_opts.keyed_nodes, (new_node, i) => {
-        const old_node = old_opts.keyed_nodes[i];
-        if (new_node.eq(old_node)) {
+    _.each(new_opts.keyed_children, (new_child, i) => {
+        const old_child = old_opts.keyed_children[i];
+        if (exports.eq_tag(new_child.ast, old_child.ast)) {
             return;
         }
-        const rendered_dom = new_node.render();
+        const rendered_dom = exports.render(new_child.ast);
         child_elems.eq(i).replaceWith(rendered_dom);
     });
 };
