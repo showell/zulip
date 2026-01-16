a087b75ea65f5aba9a99bee48cca45b112849340
Steve Howell <showell30@yahoo.com>
2026-01-16 11:15:30 -0500

    steve: Put all containers in same outer div.

--- web/showcase_src/ui.ts
+++ web/showcase_src/ui.ts
@@ -1,22 +1,37 @@
 export class Section {
+    outer_div: HTMLElement;
+
+    constructor() {
+        const outer_div = document.createElement("div");
+        outer_div.style.display = "grid";
+        outer_div.style.justifyContent = "space-around";
+        outer_div.style.backgroundColor = "white";
+
+        const demo_area = document.querySelector(".demo");
+        demo_area!.append(outer_div);
+        this.outer_div = outer_div;
+    }
+
     new_container(info: {title: string; width: string}): HTMLElement {
         const {title, width} = info;
 
-        const demo_area = document.querySelector(".demo");
-        const outer_div = document.createElement("div");
-        outer_div.style.display = "grid";
-        outer_div.style.placeItems = "center";
-        outer_div.style.backgroundColor = "white";
         const div = document.createElement("div");
+        div.style.flex = "1";
         div.style.width = width;
         div.style.border = "1px solid blue";
         div.style.margin = "5px";
         div.style.padding = "10px";
-        const heading = document.createElement("h5");
+
+        const heading = document.createElement("b");
         heading.textContent = title;
-        outer_div.append(heading);
-        outer_div.append(div);
-        demo_area!.append(outer_div);
-        return div;
+
+        const container = document.createElement("div");
+
+        div.append(heading);
+        div.append(container);
+
+        this.outer_div.append(div);
+
+        return container;
     }
 }
